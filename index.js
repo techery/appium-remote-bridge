67e563b1bc70effec518359fe7e147867cc986ac
Sergey Zenchenko <zen@Sergeys-MacBook-12.local>
2016-06-01 11:48:34 +0300

    Set status code 666 for unknown routes for testing.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var URL = require('url');
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
+    response.writeHead(666);
     response.end();
 });
 
