1e9d5092d44ac3709628bb3d4193b0d30184ca72
dirong <alex.vl.kozlov@gmail.com>
2016-06-30 12:45:58 +0300

    Add action `getTime`

--- client.js
+++ client.js
@@ -2,12 +2,24 @@ var WebSocketClient = require('websocket').client;
 
 var client = new WebSocketClient();
 
+
+var responses = {
+    getTime : function(){
+        return {time: new Date()};
+    }
+}
+
 client.on('connectFailed', function(error) {
     console.log('Connect Error: ' + error.toString());
 });
 
 client.on('connect', function(connection) {
     console.log('WebSocket Client Connected');
+
+    function sendResponse(response){
+        connection.sendUTF(JSON.stringify(response));
+    }
+
     connection.on('error', function(error) {
         console.log("Connection Error: " + error.toString());
     });
@@ -17,17 +29,12 @@ client.on('connect', function(connection) {
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log("Received: '" + message.utf8Data + "'");
+            var request = JSON.parse(message.utf8Data);
+            var responseData = responses[request.action](request.data);
+            sendResponse({action: request.action, id: request.id, data: responseData});
         }
     });
 
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
-    }
-    sendNumber();
 });
 
 client.connect('ws://localhost:8080/create?id=123', 'remote-bridge-protocol');
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var URL = require('url');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(666);
     response.end();
 });
 
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
@@ -23,18 +23,27 @@ wsServer = new WebSocketServer({
 });
 
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
 }
 
 var sessions = {};
 
-wsServer.on('request', function(request) {
+var pendingRequests = {};
+
+function initSession(id) {
+    sessions[id] = {
+        deviceConnection: null,
+        clientConnection: null
+    }
+}
+
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
 
     var connection = request.accept('remote-bridge-protocol', request.origin);
@@ -44,44 +53,56 @@ wsServer.on('request', function(request) {
     var id = urlInfo.query["id"];
 
     if (action === "/create") {
-        
+
         if (!sessions[id]) {
-            sessions[id] = {
-                deviceConnection: connection,
-                clientConnection: null
-            }
+            initSession(id);
         }
 
-        connection.on('message', function(message) {
-            sessions[id].clientConnection.sendUTF(message.utf8Data)
+        sessions[id].deviceConnection = connection;
+        if (pendingRequests[id]) {
+            pendingRequests[id].forEach(function (message) {
+                console.log('Send pending message ' + message.utf8Data);
+                if (sessions[id].deviceConnection) {
+                    sessions[id].deviceConnection.sendUTF(message.utf8Data)
+                }
+            });
+            pendingRequests[id] = null;
+        }
+
+        connection.on('message', function (message) {
+            console.log('Received response ' + message.utf8Data);
+            if (sessions[id].clientConnection) {
+                sessions[id].clientConnection.sendUTF(message.utf8Data)
+            }
         });
 
-        connection.on('close', function(reasonCode, description) {
+        connection.on('close', function (reasonCode, description) {
             sessions[id] = null;
             console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
         });
 
         console.log((new Date()) + ' Device Connection accepted.');
-    } else if (action === "/join") {
 
-        if (!sessions[id]) {
-            connection.close();
-            return;
-        }
+    } else if (action === "/connect") {
 
         console.log((new Date()) + ' Client Connection accepted.');
 
+        if (!sessions[id]) {
+            initSession(id);
+        }
+
         sessions[id].clientConnection = connection;
 
-        connection.on('message', function(message) {
-            if (sessions[id]) {
+        connection.on('message', function (message) {
+            console.log("Received request " + message);
+            if (sessions[id].deviceConnection) {
                 sessions[id].deviceConnection.sendUTF(message.utf8Data)
             } else {
-                connection.close();
-            }             
+                pendingRequests[id] = [message];
+            }
         });
 
-        connection.on('close', function(reasonCode, description) {
+        connection.on('close', function (reasonCode, description) {
             if (sessions[id]) {
                 sessions[id].clientConnection = null;
             }
